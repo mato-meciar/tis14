f1d82e2d5978b2a50f908139edd071507c4fccfc
Martin Mečiar <mato.meciar@gmail.com>
2015-02-10 14:56:46 +0100

    Update functions.php

--- aplikacia/functions/functions.php
+++ aplikacia/functions/functions.php
@@ -15,61 +15,61 @@ function hideMail($email) {
 function katedra($kat) {
 	switch ($kat) {
 		case "KAGDM":
-			return "<a href='http://www.ddm.fmph.uniba.sk/index-KATEDRA.html'>KAGDM";
+			return "<a href='http://www.ddm.fmph.uniba.sk/index-KATEDRA.html'>KAGDM target='_blank'";
 			break;
 		case "KAMS":
-			return "<a href='http://www.iam.fmph.uniba.sk/index-sk.html'>KAMS";
+			return "<a href='http://www.iam.fmph.uniba.sk/index-sk.html' target='_blank'>KAMS";
 			break;
 		case "KMANM":
-			return "<a href='http://hore.dnom.fmph.uniba.sk/'>KMANM";
+			return "<a href='http://hore.dnom.fmph.uniba.sk/' target='_blank'>KMANM";
 			break;
 		case "KAFZM":
-			return "<a href='http://www.fyzikazeme.sk/kafzm/'>KAFZM";
+			return "<a href='http://www.fyzikazeme.sk/kafzm/' target='_blank'>KAFZM";
 			break;
 		case "KEF":
-			return "<a href='http://www.dep.fmph.uniba.sk/mambo/'>KEF";
+			return "<a href='http://www.dep.fmph.uniba.sk/mambo/' target='_blank'>KEF";
 			break;
 		case "KJFB":
-			return "<a href='http://www.dnp.fmph.uniba.sk/index-sk.html'>KJFB";
+			return "<a href='http://www.dnp.fmph.uniba.sk/index-sk.html' target='_blank'>KJFB";
 			break;
 		case "KTFDF":
-			return "<a href='http://www.ddp.fmph.uniba.sk/'>KTFDF";
+			return "<a href='http://www.ddp.fmph.uniba.sk/' target='_blank'>KTFDF";
 			break;
 		case "KAI":
-			return "<a href='http://dai.fmph.uniba.sk/'>KAI";
+			return "<a href='http://dai.fmph.uniba.sk/' target='_blank'>KAI";
 			break;
 		case "KI":
-			return "<a href='http://www.dcs.fmph.uniba.sk/'>KI";
+			return "<a href='http://www.dcs.fmph.uniba.sk/' target='_blank'>KI";
 			break;
 		case "KZVI":
-			return "<a href='http://www.edi.fmph.uniba.sk/'>KZVI";
+			return "<a href='http://www.edi.fmph.uniba.sk/' target='_blank'>KZVI";
 			break;
 		case "KJP":
-			return "<a href='http://www.fmph.uniba.sk/index.php?id=1719'>KJP";
+			return "<a href='http://www.fmph.uniba.sk/index.php?id=1719' target='_blank'>KJP";
 			break;
 		case "KTVS":
-			return "<a href='http://ktvs.fmph.uniba.sk/'>KTVS";
+			return "<a href='http://ktvs.fmph.uniba.sk/' target='_blank'>KTVS";
 			break;
 		case "DEK":
-			return "<a href='http://www.fmph.uniba.sk/index.php?id=548'>DEK";
+			return "<a href='http://www.fmph.uniba.sk/index.php?id=548' target='_blank'>DEK";
 			break;
 		case "VC":
-			return "<a href='http://www.fmph.uniba.sk/index.php?id=549'>VC";
+			return "<a href='http://www.fmph.uniba.sk/index.php?id=549' target='_blank'>VC";
 			break;
 		case "KEC":
-			return "<a href='http://www.fmph.uniba.sk/index.php?id=550'>KEC";
+			return "<a href='http://www.fmph.uniba.sk/index.php?id=550' target='_blank'>KEC";
 			break;
 		case "CPP":
-			return "<a href='http://fyzikus.fmph.uniba.sk/cpp/'>CPP";
+			return "<a href='http://fyzikus.fmph.uniba.sk/cpp/' target='_blank'>CPP";
 			break;
 		case "CEF":
-			return "<a href='http://www.fmph.uniba.sk/index.php?id=554'>CEF";
+			return "<a href='http://www.fmph.uniba.sk/index.php?id=554' target='_blank'>CEF";
 			break;
 		case "VL":
-			return "<a href='http://www.fmph.uniba.sk/index.php?id=555'>VL";
+			return "<a href='http://www.fmph.uniba.sk/index.php?id=555' target='_blank'>VL";
 			break;
 		case "SB":
-			return "<a href='http://www.fmph.uniba.sk/index.php?id=3214'>SB";
+			return "<a href='http://www.fmph.uniba.sk/index.php?id=3214' target='_blank'>SB";
 			break;			
 	}
 }
