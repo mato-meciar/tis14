e2a81d339978335e1f11cf3e30a9237d16b7d234
Martin Mečiar <mato.meciar@gmail.com>
2015-02-10 17:03:06 +0100

    Update vypisUdaje.php

--- aplikacia/classes/vypisUdaje.php
+++ aplikacia/classes/vypisUdaje.php
@@ -5,22 +5,22 @@
 		private static $_table;
 
 		private function __construct($data){
-			$this->_table = '<table border="9" class="_table" align="center" style="font-size:large"; BORDERCOLOR=black>' . "<tr><th>Meno</th><th>email</th><th>Miestnost</th><th>Klapka</th><th>Katedra</th></tr>";
+			self::$_table = '<table border="9" class="_table" align="center" style="font-size:large"; BORDERCOLOR=black>' . "<tr><th>Meno</th><th>email</th><th>Miestnost</th><th>Klapka</th><th>Katedra</th></tr>";
 			foreach ($data as $key) {
-				$this->_table .= "<tr><td><a href='https://candle.fmph.uniba.sk/ucitelia/".ucfirst($key->meno)."-".ucfirst($key->priezvisko)."' target='_blank'>".ucfirst($key->meno)." ".ucfirst($key->priezvisko)."</td><td>";
-				$this->_table .= hideMail($key->email);
-				$this->_table .= "</td><td>".$key->miestnost."</td><td>".$key->klapka."</td><td>".katedra($key->katedra)."</td></tr>";
+				self::$_table .= "<tr><td><a href='https://candle.fmph.uniba.sk/ucitelia/".ucfirst($key->meno)."-".ucfirst($key->priezvisko)."' target='_blank'>".ucfirst($key->meno)." ".ucfirst($key->priezvisko)."</td><td>";
+				self::$_table .= hideMail($key->email);
+				self::$_table .= "</td><td>".$key->miestnost."</td><td>".$key->klapka."</td><td>".katedra($key->katedra)."</td></tr>";
 			}
-			$this->_table .= "</table>";
+			self::$_table .= "</table>";
 		}
 
 		public static function createTable($data){
-        	self::$_table = new vypisUdaje($data);
-      		return self::$_table;
+        	$new_table = new vypisUdaje($data);
+      		return $new_table;
     	}
 
     	public function show() {
-    		echo $this->_table;
+    		echo self::$_table;
     	}
 
     	public function getCSV($data) {
