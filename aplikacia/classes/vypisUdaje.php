c678e8c6f196af56f7b5a993812a0fe9c53db992
Martin Mečiar <mato.meciar@gmail.com>
2015-02-10 14:57:40 +0100

    Update vypisUdaje.php

--- aplikacia/classes/vypisUdaje.php
+++ aplikacia/classes/vypisUdaje.php
@@ -7,7 +7,7 @@
 		private function __construct($data){
 			$this->_table = '<table border="9" class="_table" align="center" style="font-size:large"; BORDERCOLOR=black>' . "<tr><th>Meno</th><th>email</th><th>Miestnost</th><th>Klapka</th><th>Katedra</th></tr>";
 			foreach ($data as $key) {
-				$this->_table .= "<tr><td><a href='https://candle.fmph.uniba.sk/ucitelia/".ucfirst($key->meno)."-".ucfirst($key->priezvisko)."'>".ucfirst($key->meno)." ".ucfirst($key->priezvisko)."</td><td>";
+				$this->_table .= "<tr><td><a href='https://candle.fmph.uniba.sk/ucitelia/".ucfirst($key->meno)."-".ucfirst($key->priezvisko)."' target='_blank'>".ucfirst($key->meno)." ".ucfirst($key->priezvisko)."</td><td>";
 				$this->_table .= hideMail($key->email);
 				$this->_table .= "</td><td>".$key->miestnost."</td><td>".$key->klapka."</td><td>".katedra($key->katedra)."</td></tr>";
 			}
